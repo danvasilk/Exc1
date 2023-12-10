96ed3261f35485ac259d005d50ea06b0286a48fc
danvasilk <88062672+danvasilk@users.noreply.github.com>
2023-12-10 21:58:28 +0300

    Update car.routes.js

--- routes/car.routes.js
+++ routes/car.routes.js
@@ -4,11 +4,7 @@ const carController = require('../controllers/car.controller')
 
 router.post('/car', carController.createCar)
 router.get('/car', carController.getCars)
-router.get('/car', carController.getCarsByUser)
+router.get('/car/:id', carController.getCarsByUser)
 router.delete('/car/:id', carController.deleteCar)
 
 module.exports = router
-
-
-
-
